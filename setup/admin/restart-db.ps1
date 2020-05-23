f0e9d030647240b119198ea075e30fba10e6f167
Steve Salas <ssalas@codedx.com>
2020-05-22 20:49:09 -0400

    Add script to automate MariaDb database restart

---
+++ setup/admin/restart-db.ps1
@@ -0,0 +1,79 @@
+<#PSScriptInfo
+.VERSION 1.0.0
+.GUID a48bc8e0-dada-4b63-944a-9397ce91f0b3
+.AUTHOR Code Dx
+#>
+
+<# 
+.DESCRIPTION 
+This script automates the process of restarting the MariaDB databases.
+#>
+
+
+param (
+	[string] $namespaceCodeDx = 'cdx-app',
+	[string] $releaseNameCodeDx = 'codedx',
+	[int]    $waitSeconds = 600
+)
+
+$ErrorActionPreference = 'Stop'
+$VerbosePreference = 'Continue'
+
+Set-PSDebug -Strict
+
+. (join-path $PSScriptRoot '../common/k8s.ps1')
+. (join-path $PSScriptRoot '../common/helm.ps1')
+. (join-path $PSScriptRoot '../common/codedx.ps1')
+
+if (-not (Test-HelmRelease $namespaceCodeDx $releaseNameCodeDx)) {
+	Write-Error "Unable to find Helm release named $releaseNameCodeDx in namespace $namespaceCodeDx."
+}
+
+$deploymentCodeDx = Get-CodeDxChartFullName $releaseNameCodeDx
+$statefulSetMariaDBMaster = "$releaseNameCodeDx-mariadb-master"
+$statefulSetMariaDBSlave = "$releaseNameCodeDx-mariadb-slave"
+
+if (-not (Test-Deployment $namespaceCodeDx $deploymentCodeDx)) {
+	Write-Error "Unable to find Deployment named $deploymentCodeDx in namespace $namespaceCodeDx."
+}
+
+if (-not (Test-StatefulSet $namespaceCodeDx $statefulSetMariaDBMaster)) {
+	Write-Error "Unable to find StatefulSet named $statefulSetMariaDBMaster in namespace $namespaceCodeDx."
+}
+
+if (-not (Test-StatefulSet $namespaceCodeDx $statefulSetMariaDBSlave)) {
+	Write-Error "Unable to find StatefulSet named $statefulSetMariaDBSlave in namespace $namespaceCodeDx."
+}
+
+$statefulSetMariaDBSlaveCount = (Get-HelmValues $namespaceCodeDx $releaseNameCodeDx).mariadb.slave.replicas
+
+Write-Host @"
+
+Using the following configuration:
+
+Code Dx Deployment Name: $deploymentCodeDx
+MariaDB Master StatefulSet Name: $statefulSetMariaDBMaster
+MariaDB Slave StatefulSet Name: $statefulSetMariaDBSlave
+MariaDB Slave Replica Count: $statefulSetMariaDBSlaveCount
+
+"@
+
+Write-Verbose "Stopping Code Dx deployment named $deploymentCodeDx..."
+Set-DeploymentReplicas  $namespaceCodeDx $deploymentCodeDx 0 $waitSeconds
+
+Write-Verbose "Stopping $statefulSetMariaDBMaster statefulset replica..."
+Set-StatefulSetReplicas $namespaceCodeDx $statefulSetMariaDBMaster 0 $waitSeconds
+
+Write-Verbose "Stopping $statefulSetMariaDBSlave statefulset replica(s)..."
+Set-StatefulSetReplicas $namespaceCodeDx $statefulSetMariaDBSlave 0 $waitSeconds
+
+Write-Verbose "Starting $statefulSetMariaDBMaster statefulset replica..."
+Set-StatefulSetReplicas $namespaceCodeDx $statefulSetMariaDBMaster 1 $waitSeconds
+
+Write-Verbose "Starting $statefulSetMariaDBSlave statefulset replica(s)..."
+Set-StatefulSetReplicas $namespaceCodeDx $statefulSetMariaDBSlave $statefulSetMariaDBSlaveCount $waitSeconds
+
+Write-Verbose "Starting Code Dx deployment named $deploymentCodeDx..."
+Set-DeploymentReplicas  $namespaceCodeDx $deploymentCodeDx 1 $waitSeconds
+
+Write-Host 'Done'

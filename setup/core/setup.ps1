41f5788cdcc0140c3a6c972d258def236e2feb14
Steve Salas <ssalas@codedx.com>
2020-09-08 16:55:59 -0400

    Version for v1.3.6 release

--- setup/core/setup.ps1
+++ setup/core/setup.ps1
@@ -111,7 +111,7 @@ param (
 	[string]   $codedxHelmRepo = 'https://codedx.github.io/codedx-kubernetes',
 	
 	[string]   $codedxGitRepo = 'https://github.com/codedx/codedx-kubernetes.git',
-	[string]   $codedxGitRepoBranch = 'v1.3.5',
+	[string]   $codedxGitRepoBranch = 'v1.3.6',
 
 	[int]      $kubeApiTargetPort = 443,
 

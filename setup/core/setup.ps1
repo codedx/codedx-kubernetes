c0d9853dd0adf2d132ff56e36b86f5aa0ad9fab2
Steve Salas <ssalas@codedx.com>
2020-10-14 16:46:31 -0400

    Update to Code Dx v5.2.4

--- setup/core/setup.ps1
+++ setup/core/setup.ps1
@@ -1,5 +1,5 @@
 <#PSScriptInfo
-.VERSION 1.4.3
+.VERSION 1.4.4
 .GUID 47733b28-676e-455d-b7e8-88362f442aa3
 .AUTHOR Code Dx
 #>
@@ -52,7 +52,7 @@ param (
 	[string]   $workflowEphemeralStorageReservation,
 	[string]   $nginxEphemeralStorageReservation,
 
-	[string]   $imageCodeDxTomcat = 'codedx/codedx-tomcat:v5.2.3',
+	[string]   $imageCodeDxTomcat = 'codedx/codedx-tomcat:v5.2.4',
 	[string]   $imageCodeDxTools = 'codedx/codedx-tools:v1.1.0',
 	[string]   $imageCodeDxToolsMono = 'codedx/codedx-toolsmono:v1.1.0',
 	[string]   $imageNewAnalysis = 'codedx/codedx-newanalysis:v1.1.0',

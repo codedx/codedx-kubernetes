c7c9c3cb4ac9ae0936f7314603ac347215aa0ba2
Steve Salas <ssalas@codedx.com>
2020-10-20 11:08:28 -0400

    Set latest version to v1.4.5

--- setup/core/setup.ps1
+++ setup/core/setup.ps1
@@ -111,7 +111,7 @@ param (
 	[string]   $codedxHelmRepo = 'https://codedx.github.io/codedx-kubernetes',
 	
 	[string]   $codedxGitRepo = 'https://github.com/codedx/codedx-kubernetes.git',
-	[string]   $codedxGitRepoBranch = 'v1.4.3',
+	[string]   $codedxGitRepoBranch = 'v1.4.5',
 
 	[int]      $kubeApiTargetPort = 443,
 

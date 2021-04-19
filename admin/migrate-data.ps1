9f6786aac8df08ae3f20eaa63ef3d8fcc5505a4a
Steve Salas <ssalas@codedx.com>
2021-04-19 17:19:51 -0400

    feat: data migration w/ external database support

--- admin/migrate-data.ps1
+++ admin/migrate-data.ps1
@@ -1,5 +1,5 @@
 <#PSScriptInfo
-.VERSION 1.0.2
+.VERSION 1.1.0
 .GUID 1830f430-23af-46c2-b73c-8b936957b671
 .AUTHOR Code Dx
 #>
@@ -7,8 +7,8 @@
 <# 
 .DESCRIPTION 
 This script helps you migrate Code Dx data from a system created by the 
-Code Dx Installer to a Code Dx deployment running on k8s (without 
-an external database).
+Code Dx Installer to a Code Dx deployment running on k8s (with or 
+without an external database).
 #>
 
 
@@ -16,9 +16,11 @@ param (
 	[string] $appDataPath,
 	[string] $dumpFile,
 	[string] $rootPwd,
+	[string] $replicationPwd,
 	[string] $namespaceCodeDx = 'cdx-app',
 	[string] $releaseNameCodeDx = 'codedx',
-	[int]    $waitSeconds = 600
+	[int]    $waitSeconds = 600,
+	[switch] $externalDatabase
 )
 
 $ErrorActionPreference = 'Stop'
@@ -34,6 +36,8 @@ Set-PSDebug -Strict
 	. $path
 }
 
+$internalDatabase = -not $externalDatabase
+
 if ($appDataPath -eq '') { 
 	$appDataPath = Read-HostText 'Enter the path to your Code Dx AppData folder' 1 
 }
@@ -47,16 +51,31 @@ if (-not (Test-Path $analysisFiles -PathType Container)) {
 	Write-Error "Unable to find Code Dx AppData analysis-files folder at $analysisFiles."
 }
 
-if ($dumpFile -eq '') { 
-	$dumpFile = Read-HostText 'Enter the path to your mysqldump file' 1 
+if ($internalDatabase) {
+
+	if ($dumpFile -eq '') {
+		$dumpFile = Read-HostText 'Enter the path to your mysqldump file' 1
+	}
+
+	if (-not (Test-Path $dumpFile -PathType Leaf)) {
+		Write-Error "Unable to find mysqldumpfile at $dumpFile."
+	}
+
+	if ($rootPwd -eq '') {
+		$rootPwd = Read-HostSecureText 'Enter the password for the MariaDB root user' 1
+	}
+
+	if ($replicationPwd -eq '') {
+		$replicationPwd = Read-HostSecureText 'Enter the password for the MariaDB replicator user' 1
+	}
 }
 
-if (-not (Test-Path $dumpFile -PathType Leaf)) {
-	Write-Error "Unable to find mysqldumpfile at $dumpFile."
+if (-not (Test-Namespace $namespaceCodeDx)) {
+	$namespaceCodeDx = Read-HostText 'Enter the Code Dx k8s namespace' 1
 }
 
-if ($rootPwd -eq '') { 
-	$rootPwd = Read-HostSecureText 'Enter a password for the MariaDB root user' 1 
+if (-not (Test-HelmRelease $namespaceCodeDx $releaseNameCodeDx)) {
+	$releaseNameCodeDx = Read-HostText "Enter the Code Dx Helm release name in the $namespaceCodeDx namespace" 1
 }
 
 if (-not (Test-HelmRelease $namespaceCodeDx $releaseNameCodeDx)) {
@@ -64,60 +83,79 @@ if (-not (Test-HelmRelease $namespaceCodeDx $releaseNameCodeDx)) {
 }
 
 $deploymentCodeDx = Get-CodeDxChartFullName $releaseNameCodeDx
-$statefulSetMariaDBMaster = "$releaseNameCodeDx-mariadb-master"
-$statefulSetMariaDBSlave = "$releaseNameCodeDx-mariadb-slave"
-$mariaDbSecretName = "$releaseNameCodeDx-mariadb-pd"
-$mariaDbMasterServiceName = "$releaseNameCodeDx-mariadb"
+
+$statefulSetMariaDBMaster = ''
+$statefulSetMariaDBSlave  = ''
+$mariaDbSecretName        = ''
+$mariaDbMasterServiceName = ''
+
+if ($internalDatabase) {
+
+	$statefulSetMariaDBMaster = "$releaseNameCodeDx-mariadb-master"
+	$statefulSetMariaDBSlave  = "$releaseNameCodeDx-mariadb-slave"
+	$mariaDbSecretName        = "$releaseNameCodeDx-mariadb-pd"
+	$mariaDbMasterServiceName = "$releaseNameCodeDx-mariadb"
+}
 
 if (-not (Test-Deployment $namespaceCodeDx $deploymentCodeDx)) {
 	Write-Error "Unable to find Deployment named $deploymentCodeDx in namespace $namespaceCodeDx."
 }
 
-if (-not (Test-StatefulSet $namespaceCodeDx $statefulSetMariaDBMaster)) {
-	Write-Error "Unable to find StatefulSet named $statefulSetMariaDBMaster in namespace $namespaceCodeDx."
+if ($internalDatabase) {
+
+	if (-not (Test-StatefulSet $namespaceCodeDx $statefulSetMariaDBMaster)) {
+		Write-Error "Unable to find StatefulSet named $statefulSetMariaDBMaster in namespace $namespaceCodeDx."
+	}
+
+	if (-not (Test-StatefulSet $namespaceCodeDx $statefulSetMariaDBSlave)) {
+		Write-Error "Unable to find StatefulSet named $statefulSetMariaDBSlave in namespace $namespaceCodeDx."
+	}
+
+	if (-not (Test-Secret $namespaceCodeDx $mariaDbSecretName)) {
+		Write-Error "Unable to find Secret named $mariaDbSecretName in namespace $namespaceCodeDx."
+	}
+
+	if (-not (Test-Service $namespaceCodeDx $mariaDbMasterServiceName)) {
+		Write-Error "Unable to find Service named $mariaDbMasterServiceName in namespace $namespaceCodeDx."
+	}
 }
 
-if (-not (Test-StatefulSet $namespaceCodeDx $statefulSetMariaDBSlave)) {
-	Write-Error "Unable to find StatefulSet named $statefulSetMariaDBSlave in namespace $namespaceCodeDx."
+$statefulSetMariaDBSlaveCount = 0
+if ($internalDatabase) {
+	$statefulSetMariaDBSlaveCount = (Get-HelmValues $namespaceCodeDx $releaseNameCodeDx).mariadb.slave.replicas
 }
-
-if (-not (Test-Secret $namespaceCodeDx $mariaDbSecretName)) {
-	Write-Error "Unable to find Secret named $mariaDbSecretName in namespace $namespaceCodeDx."
-}
-
-if (-not (Test-Service $namespaceCodeDx $mariaDbMasterServiceName)) {
-	Write-Error "Unable to find Service named $mariaDbMasterServiceName in namespace $namespaceCodeDx."
-}
-
-$statefulSetMariaDBSlaveCount = (Get-HelmValues $namespaceCodeDx $releaseNameCodeDx).mariadb.slave.replicas
 
 Write-Host @"
 
 Using the following configuration:
 
-Code Dx Deployment Name: $deploymentCodeDx
+Code Dx Deployment Name:         $deploymentCodeDx
+External Database:               $externalDatabase
 MariaDB Master StatefulSet Name: $statefulSetMariaDBMaster
-MariaDB Slave StatefulSet Name: $statefulSetMariaDBSlave
-MariaDB Slave Replica Count: $statefulSetMariaDBSlaveCount
-MariaDB Secret Name: $mariaDbSecretName
-MariaDB Master Service Name: $mariaDbMasterServiceName
+MariaDB Slave StatefulSet Name:  $statefulSetMariaDBSlave
+MariaDB Slave Replica Count:     $statefulSetMariaDBSlaveCount
+MariaDB Secret Name:             $mariaDbSecretName
+MariaDB Master Service Name:     $mariaDbMasterServiceName
 "@
 
-Write-Verbose 'Searching for MariaDB slave pods...'
-$podFullNamesSlaves = kubectl -n $namespaceCodeDx get pod -l component=slave -o name
-if (0 -ne $LASTEXITCODE) {
-	Write-Error "Unable to fetch slave pods, kubectl exited with exit code $LASTEXITCODE."
-}
+if ($internalDatabase) {
 
-$podNamesSlaves = @()
-$podFullNamesSlaves | ForEach-Object {
+	Write-Verbose 'Searching for MariaDB slave pods...'
+	$podFullNamesSlaves = kubectl -n $namespaceCodeDx get pod -l component=slave -o name
+	if (0 -ne $LASTEXITCODE) {
+		Write-Error "Unable to fetch slave pods, kubectl exited with exit code $LASTEXITCODE."
+	}
 
-	$podName = $_ -replace 'pod/',''
-	$podNamesSlaves = $podNamesSlaves + $podName
-}
+	$podNamesSlaves = @()
+	$podFullNamesSlaves | ForEach-Object {
 
-if ($statefulSetMariaDBSlaveCount -ne $podNamesSlaves.Count) {
-	Write-Error "Make sure all subordinate databases are running and retry."
+		$podName = $_ -replace 'pod/',''
+		$podNamesSlaves = $podNamesSlaves + $podName
+	}
+
+	if ($statefulSetMariaDBSlaveCount -ne $podNamesSlaves.Count) {
+		Write-Error "Make sure all subordinate databases are running and retry."
+	}
 }
 
 Write-Verbose 'Searching for Code Dx pods...'
@@ -127,42 +165,64 @@ if (0 -ne $LASTEXITCODE) {
 }
 $podNameCodeDx = $podNameCodeDx -replace 'pod/',''
 
-Write-Verbose 'Searching for MariaDB master pod...'
-$podNameMaster = kubectl -n $namespaceCodeDx get pod -l component=master -o name
-if (0 -ne $LASTEXITCODE) {
-	Write-Error "Unable to find MariaDB master pod, kubectl exited with exit code $LASTEXITCODE."
-}
-$podNameMaster = $podNameMaster -replace 'pod/',''
+if ($internalDatabase) {
 
-if (-not (Test-Database $namespaceCodeDx $podNameMaster 'mariadb' $rootPwd)) {
-	Write-Error "Unable to log on to the MariaDB server as root. Is the password correct?"
+	Write-Verbose 'Searching for MariaDB master pod...'
+	$podNameMaster = kubectl -n $namespaceCodeDx get pod -l component=master -o name
+	if (0 -ne $LASTEXITCODE) {
+		Write-Error "Unable to find MariaDB master pod, kubectl exited with exit code $LASTEXITCODE."
+	}
+	$podNameMaster = $podNameMaster -replace 'pod/',''
+
+	if (-not (Test-Database $namespaceCodeDx $podNameMaster 'mariadb' $rootPwd)) {
+		Write-Error "Unable to log on to the MariaDB server as root. Is the password correct?"
+	}
 }
 
 Write-Verbose "Stopping Code Dx deployment named $deploymentCodeDx..."
 Set-DeploymentReplicas  $namespaceCodeDx $deploymentCodeDx 0 $waitSeconds
 
-Write-Verbose 'Stopping slave database instances...'
-$podNamesSlaves | ForEach-Object {
-	Write-Verbose "Stopping slave named $podName..."
-	Stop-SlaveDB $namespaceCodeDx $podName 'mariadb' $rootPwd
-}
+if ($internalDatabase) {
 
-Write-Verbose "Restoring database on pod $podNameMaster..."
-New-Database $namespaceCodeDx $podNameMaster 'mariadb' $rootPwd 'codedx' $dumpFile
+	Write-Verbose 'Stopping slave database instances...'
+	$podNamesSlaves | ForEach-Object {
+		Write-Verbose "Stopping slave named $podName..."
+		Stop-SlaveDB $namespaceCodeDx $podName 'mariadb' $rootPwd
+	}
 
-$podNamesSlaves | ForEach-Object {
-	Write-Verbose "Restoring database on pod $_..."
-	New-Database $namespaceCodeDx $_ 'mariadb' $rootPwd 'codedx' $dumpFile
-}
+	Write-Verbose "Restoring database on pod $podNameMaster..."
+	New-Database $namespaceCodeDx $podNameMaster 'mariadb' $rootPwd 'codedx' $dumpFile
 
-Write-Verbose 'Resetting master database...'
-$filePos = Get-MasterFilePosAfterReset $namespaceCodeDx 'mariadb' $podNameMaster $rootPwd
+	$podNamesSlaves | ForEach-Object {
+		Write-Verbose "Restoring database on pod $_..."
+		New-Database $namespaceCodeDx $_ 'mariadb' $rootPwd 'codedx' $dumpFile
+	}
 
-Write-Verbose 'Connecting slave database(s)...'
-$podNamesSlaves | ForEach-Object {
-	Write-Verbose "Restoring slave database pod $_..."
-	Stop-SlaveDB  $namespaceCodeDx $_ 'mariadb' $rootPwd
-	Start-SlaveDB $namespaceCodeDx $_ 'mariadb' $rootPwd $mariaDbMasterServiceName $filePos
+	Write-Verbose 'Resetting master database...'
+	$filePos = Get-MasterFilePosAfterReset $namespaceCodeDx 'mariadb' $podNameMaster $rootPwd
+
+	Write-Verbose 'Connecting slave database(s)...'
+	$podNamesSlaves | ForEach-Object {
+		Write-Verbose "Restoring slave database pod $_..."
+		Stop-SlaveDB  $namespaceCodeDx $_ 'mariadb' $rootPwd
+		Start-SlaveDB $namespaceCodeDx $_ 'mariadb' 'replicator' $replicationPwd $rootPwd $mariaDbMasterServiceName $filePos
+	}
+} else {
+
+	$externalDatabaseInstructions = @'
+
+Since your Code Dx Kubernetes deployment uses an external Code Dx database (one
+that you maintain on your own that is not installed or updated by the Code Dx
+Kubernetes deployment script), you must restore the dump-codedx.sql file you 
+created in Step 2 of the Migrate Code Dx Data to Kubernetes instructions.
+
+You can restore mysqldump files using a command that looks like this:
+
+  mysql -uroot --password=root-password codedxdb < dump-codedx.sql
+
+'@
+	Write-Host $externalDatabaseInstructions
+	Read-Host -Prompt 'Restore your database (see above) and then press Enter to continue...'
 }
 
 Write-Verbose "Starting Code Dx deployment named $deploymentCodeDx..."
--- setup/core/docs/config/migrate-data-external-db.md
+++ setup/core/docs/config/migrate-data-external-db.md
@@ -1,12 +1,10 @@
-# Migrate Code Dx Data to Kubernetes
+# Migrate Code Dx Data to Kubernetes (External Database)
 
-Here are the steps to migrate your Code Dx data from a system created by the Code Dx Installer to a Code Dx deployment running on Kubernetes (without an external database).
-
->Note: If your Code Dx Kubernetes deployment uses an external database (one that you maintain on your own that is not installed or updated by the Code Dx Kubernetes deployment script), follow the [Migrate Code Dx Data to Kubernetes (External Database)](migrate-data-external-db.md) instructions instead.
-
-If you have not yet installed Code Dx on Kubernetes, run the [Guided Setup](https://github.com/codedx/codedx-kubernetes#download-guided-setup) and the Code Dx deployment script at this time.
+Here are the steps to migrate your Code Dx data from a system created by the Code Dx Installer to a Code Dx deployment running on Kubernetes (with an external database).
 
 The Code Dx version you are running on Kubernetes must be equal to or greater than your non-Kubernetes Code Dx system. If necessary, upgrade your Code Dx version before migrating your Code Dx data.
+
+If you have not yet installed Code Dx on Kubernetes using an external database, run the [Guided Setup](https://github.com/codedx/codedx-kubernetes#download-guided-setup) and the Code Dx deployment script at this time.
 
 >Note: You must have adequate disk and memory resources before starting a data migration with a large Code Dx dataset.
 
@@ -30,11 +28,11 @@ mysqldump --host=127.0.0.1 --port=3306 --user=root -p codedx > dump-codedx.sql
 git clone https://github.com/codedx/codedx-kubernetes.git
 ```
 
-5) Run the migrate-data.ps1 script in the codedx-kubernetes/admin directory. When prompted, enter the path to your dump-codedx.sql file, your Code Dx AppData directory, and specify the passwords for the root and replicator database users (the passwords you specified when running the Guided Setup).
+5) Run the migrate-data.ps1 script in the codedx-kubernetes/admin directory using the -externalDatabase parameter. When prompted, enter the path to your Code Dx AppData directory.
 
 ```
 cd codedx-kubernetes/admin
-pwsh ./migrate-data.ps1
+pwsh ./migrate-data.ps1 -externalDatabase
 ```
 
 Code Dx will be ready with the migrated data once the migrate-data.ps1 script finishes.

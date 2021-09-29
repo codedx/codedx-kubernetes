6cf040449b8bd4a19d3391e0c9857b3d2661c61f
Code Dx Build <support@codedx.com>
2021-09-29 19:05:38 +0000

    feat: update to latest Docker image versions
    Code Dx: v5.5.0
    
    Tomcat Init: v1.1.0
    
    MariaDB: v1.1.1
    
    Tool Orchestration: v1.9.1
    
    Workflow: v2.13.0
    
    RestoreDB: v1.3.0

--- setup/core/setup.ps1
+++ setup/core/setup.ps1
@@ -56,9 +56,9 @@ param (
 	[string]                 $workflowEphemeralStorageReservation,
 	[string]                 $nginxEphemeralStorageReservation,
 
-	[string]                 $imageCodeDxTomcat       = 'codedx/codedx-tomcat:v5.4.15',
-	[string]                 $imageCodeDxTools        = 'codedx/codedx-tools:v5.4.15',
-	[string]                 $imageCodeDxToolsMono    = 'codedx/codedx-toolsmono:v5.4.15',
+	[string]                 $imageCodeDxTomcat       = 'codedx/codedx-tomcat:v5.5.0',
+	[string]                 $imageCodeDxTools        = 'codedx/codedx-tools:v5.5.0',
+	[string]                 $imageCodeDxToolsMono    = 'codedx/codedx-toolsmono:v5.5.0',
 
 	[string]                 $imagePrepare            = 'codedx/codedx-prepare:v1.9.1',
 	[string]                 $imageNewAnalysis        = 'codedx/codedx-newanalysis:v1.9.1',

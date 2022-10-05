faa8954e415cd5e6d6f1c99a07cc889dc4f33dab
Code Dx Build <support@codedx.com>
2022-10-05 00:42:44 +0000

    chore: set chart reference to charts-2.18.0

--- setup/core/setup.ps1
+++ setup/core/setup.ps1
@@ -124,7 +124,7 @@ param (
 	[string]                 $codedxHelmRepo = 'https://codedx.github.io/codedx-kubernetes',
 	
 	[string]                 $codedxGitRepo = 'https://github.com/codedx/codedx-kubernetes.git',
-	[string]                 $codedxGitRepoBranch = 'charts-2.17.0',
+	[string]                 $codedxGitRepoBranch = 'charts-2.18.0',
 
 	[int]                    $kubeApiTargetPort = 443,
 

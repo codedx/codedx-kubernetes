0dcc69811719c266f036065488ca49e848758329
Code Dx Build <support@codedx.com>
2024-06-11 12:05:37 +0000

    feat: update to latest Docker image versions
    Code Dx: v2024.6.0
    
    MariaDB: v1.31.0
    
    Tool Orchestration: v1.34.0
    
    Workflow: v3.0.0
    
    RestoreDB: v1.9.0

--- setup/core/setup.ps1
+++ setup/core/setup.ps1
@@ -53,9 +53,9 @@ param (
 	[string]                 $minioEphemeralStorageReservation,
 	[string]                 $workflowEphemeralStorageReservation,
 
-	[string]                 $imageCodeDxTomcat       = 'codedx/codedx-tomcat:v2024.3.5',
-	[string]                 $imageCodeDxTools        = 'codedx/codedx-tools:v2024.3.5',
-	[string]                 $imageCodeDxToolsMono    = 'codedx/codedx-toolsmono:v2024.3.5',
+	[string]                 $imageCodeDxTomcat       = 'codedx/codedx-tomcat:v2024.6.0',
+	[string]                 $imageCodeDxTools        = 'codedx/codedx-tools:v2024.6.0',
+	[string]                 $imageCodeDxToolsMono    = 'codedx/codedx-toolsmono:v2024.6.0',
 
 	[string]                 $imagePrepare            = 'codedx/codedx-prepare:v1.34.0',
 	[string]                 $imageNewAnalysis        = 'codedx/codedx-newanalysis:v1.34.0',
@@ -64,11 +64,11 @@ param (
 	[string]                 $imageToolService        = 'codedx/codedx-tool-service:v1.34.0',
 	[string]                 $imagePreDelete          = 'codedx/codedx-cleanup:v1.34.0',
 
-	[string]                 $imageCodeDxTomcatInit   = 'codedx/codedx-tomcat:v2024.3.5',
-	[string]                 $imageMariaDB            = 'codedx/codedx-mariadb:v1.30.0',
+	[string]                 $imageCodeDxTomcatInit   = 'codedx/codedx-tomcat:v2024.6.0',
+	[string]                 $imageMariaDB            = 'codedx/codedx-mariadb:v1.31.0',
 	[string]                 $imageMinio              = 'bitnami/minio:2021.4.6-debian-10-r11',
-	[string]                 $imageWorkflowController = 'codedx/codedx-workflow-controller:v2.17.0',
-	[string]                 $imageWorkflowExecutor   = 'codedx/codedx-argoexec:v2.17.0',
+	[string]                 $imageWorkflowController = 'codedx/codedx-workflow-controller:v3.0.0',
+	[string]                 $imageWorkflowExecutor   = 'codedx/codedx-argoexec:v3.0.0',
 
 	[int]                    $toolServiceReplicas = 3,
 

2e0179aa04b194558068f2ff4f68c39c7f9635fe
Code Dx Build <support@codedx.com>
2024-07-11 12:56:20 +0000

    feat: update to latest Docker image versions
    Code Dx: v2024.6.3
    
    MariaDB: v1.32.0
    
    Tool Orchestration: v1.34.0
    
    Workflow: v2.17.0
    
    RestoreDB: v1.9.0

--- setup/core/setup.ps1
+++ setup/core/setup.ps1
@@ -53,9 +53,9 @@ param (
 	[string]                 $minioEphemeralStorageReservation,
 	[string]                 $workflowEphemeralStorageReservation,
 
-	[string]                 $imageCodeDxTomcat       = 'codedx/codedx-tomcat:v2024.6.2',
-	[string]                 $imageCodeDxTools        = 'codedx/codedx-tools:v2024.6.2',
-	[string]                 $imageCodeDxToolsMono    = 'codedx/codedx-toolsmono:v2024.6.2',
+	[string]                 $imageCodeDxTomcat       = 'codedx/codedx-tomcat:v2024.6.3',
+	[string]                 $imageCodeDxTools        = 'codedx/codedx-tools:v2024.6.3',
+	[string]                 $imageCodeDxToolsMono    = 'codedx/codedx-toolsmono:v2024.6.3',
 
 	[string]                 $imagePrepare            = 'codedx/codedx-prepare:v1.34.0',
 	[string]                 $imageNewAnalysis        = 'codedx/codedx-newanalysis:v1.34.0',
@@ -64,7 +64,7 @@ param (
 	[string]                 $imageToolService        = 'codedx/codedx-tool-service:v1.34.0',
 	[string]                 $imagePreDelete          = 'codedx/codedx-cleanup:v1.34.0',
 
-	[string]                 $imageCodeDxTomcatInit   = 'codedx/codedx-tomcat:v2024.6.2',
+	[string]                 $imageCodeDxTomcatInit   = 'codedx/codedx-tomcat:v2024.6.3',
 	[string]                 $imageMariaDB            = 'codedx/codedx-mariadb:v1.32.0',
 	[string]                 $imageMinio              = 'bitnami/minio:2021.4.6-debian-10-r11',
 	[string]                 $imageWorkflowController = 'codedx/codedx-workflow-controller:v2.17.0',

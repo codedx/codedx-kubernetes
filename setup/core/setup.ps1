13b1efd7c44cc4dc337b9a143002ac23fca9146b
Code Dx Build <support@codedx.com>
2025-03-11 14:01:29 +0000

    feat: update to latest Docker image versions
    Code Dx: v2025.3.0
    
    MariaDB: v1.38.0
    
    Tool Orchestration: v1.34.0
    
    Workflow: v2.17.0
    
    RestoreDB: v1.14.0

--- setup/core/setup.ps1
+++ setup/core/setup.ps1
@@ -54,9 +54,9 @@ param (
 	[string]                 $minioEphemeralStorageReservation,
 	[string]                 $workflowEphemeralStorageReservation,
 
-	[string]                 $imageCodeDxTomcat       = 'codedx/codedx-tomcat:v2024.12.3',
-	[string]                 $imageCodeDxTools        = 'codedx/codedx-tools:v2024.12.3',
-	[string]                 $imageCodeDxToolsMono    = 'codedx/codedx-toolsmono:v2024.12.3',
+	[string]                 $imageCodeDxTomcat       = 'codedx/codedx-tomcat:v2025.3.0',
+	[string]                 $imageCodeDxTools        = 'codedx/codedx-tools:v2025.3.0',
+	[string]                 $imageCodeDxToolsMono    = 'codedx/codedx-toolsmono:v2025.3.0',
 
 	[string]                 $imagePrepare            = 'codedx/codedx-prepare:v1.34.0',
 	[string]                 $imageNewAnalysis        = 'codedx/codedx-newanalysis:v1.34.0',
@@ -65,8 +65,8 @@ param (
 	[string]                 $imageToolService        = 'codedx/codedx-tool-service:v1.34.0',
 	[string]                 $imagePreDelete          = 'codedx/codedx-cleanup:v1.34.0',
 
-	[string]                 $imageCodeDxTomcatInit   = 'codedx/codedx-tomcat:v2024.12.3',
-	[string]                 $imageMariaDB            = 'codedx/codedx-mariadb:v1.37.0',
+	[string]                 $imageCodeDxTomcatInit   = 'codedx/codedx-tomcat:v2025.3.0',
+	[string]                 $imageMariaDB            = 'codedx/codedx-mariadb:v1.38.0',
 	[string]                 $imageMinio              = 'bitnami/minio:2021.4.6-debian-10-r11',
 	[string]                 $imageWorkflowController = 'codedx/codedx-workflow-controller:v2.17.0',
 	[string]                 $imageWorkflowExecutor   = 'codedx/codedx-argoexec:v2.17.0',

a8dad4a08219772df8d7970ffa04d6572de00185
Code Dx Build <support@codedx.com>
2025-03-25 13:25:35 +0000

    chore: set chart reference to charts-2.89.0

--- setup/core/setup.ps1
+++ setup/core/setup.ps1
@@ -128,7 +128,7 @@ param (
 	[string]                 $helmTimeoutToolOrchestration = '15m0s',
 	
 	[string]                 $codedxGitRepo = 'https://github.com/codedx/codedx-kubernetes.git',
-	[string]                 $codedxGitRepoBranch = 'charts-2.88.0',
+	[string]                 $codedxGitRepoBranch = 'charts-2.89.0',
 
 	[int]                    $kubeApiTargetPort = 443,
 

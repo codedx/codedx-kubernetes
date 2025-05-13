20fcedb1bce2fe17453e04fb23adf0f996bcb3df
Code Dx Build <support@codedx.com>
2025-05-13 11:26:13 +0000

    feat: update to latest Docker image versions
    Code Dx: v2025.3.6
    
    MariaDB: v1.38.0
    
    Tool Orchestration: v1.34.0
    
    Workflow: v2.17.0
    
    RestoreDB: v1.15.0

--- admin/restore-db.ps1
+++ admin/restore-db.ps1
@@ -19,7 +19,7 @@ param (
 	[string] $namespaceCodeDx = 'cdx-app',
 	[string] $releaseNameCodeDx = 'codedx',
 	[int]    $waitSeconds = 600,
-	[string] $imageDatabaseRestore = 'codedx/codedx-dbrestore:v1.14.0',
+	[string] $imageDatabaseRestore = 'codedx/codedx-dbrestore:v1.15.0',
 	[string] $dockerImagePullSecretName,
 	[switch] $skipCodeDxRestart
 )
